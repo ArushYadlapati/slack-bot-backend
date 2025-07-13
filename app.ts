df510e57f5a9d1eafa8b2ec6c24954ed67f1f241
ArushYadlapati <arush.yadlapati@gmail.com>
2025-07-13 02:29:42 -0700

    Add a really amazing dev workflow (FINALLY!!!)

--- app.ts
+++ app.ts
@@ -1,13 +1,73 @@
-require('@dotenvx/dotenvx').config();
-const { App } = require('@slack/bolt');
+require("@dotenvx/dotenvx").config();
+const { App, AwsLambdaReceiver } = require("@slack/bolt");
 
-const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
+const awsLambdaReceiver = new AwsLambdaReceiver({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-(async () => {
-    await app.start(process.env.PORT || 3000);
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    receiver: awsLambdaReceiver
+});
 
-    app.logger.info('⚡️ Bolt app is running!');
-})();
+app.message("hello", async ({ message, say }) => {
+    await say(
+        `Yo ID ${ message.user }. Your name is <@${ message.user }>.`
+    );
+});
+
+app.message("help", async ({ message, say }) => {
+   await say(
+       `Hello <@${ message.user }>. This is the /help command.`
+   );
+});
+
+app.message("play", async ({ message, say }) => {
+    await say(
+        `Hello <@${ message.user }>. This is the /play command.`
+    );
+});
+
+app.message("leaderboard", async ({ message, say }) => {
+    await say(
+        `Hello <@${ message.user }>. This is the /leaderboard command.`
+    );
+});
+
+app.message("button", async ({ message, say }) => {
+    await say({
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: `Hey there <@${message.user}>!`,
+                },
+                accessory: {
+                    type: 'button',
+                    text: {
+                        type: 'plain_text',
+                        text: 'Click Me',
+                    },
+                    action_id: 'button_click',
+                },
+            },
+        ],
+        text: `Hey there <@${message.user}>!`,
+    });
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+    await ack();
+
+    await say(`<@${body.user.id}> clicked the button`);
+});
+
+app.message('goodbye', async ({ message, say }) => {
+    await say(`See ya later, <@${message.user}> :wave:`);
+});
+
+module.exports.handler = async (event, context, callback) => {
+    const handler = await awsLambdaReceiver.start();
+    return handler(event, context, callback);
+}
